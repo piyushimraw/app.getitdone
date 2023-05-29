77d830f170c7861b5feca5a5929a0c48ce0b7e96
piyushimraw <piyush.srivastavaimraw@gmail.com>
2023-05-29 13:02:00 +0530

    added signin and register functions

---
+++ src/lib/api.ts
@@ -0,0 +1,45 @@
+import { HTTP_METHOD } from "next/dist/server/web/http";
+
+export type FetcherParams = {
+  url: string;
+  method: HTTP_METHOD;
+  body: any;
+  json?: boolean;
+};
+export const fetcher = async ({
+  url,
+  method,
+  body,
+  json = true,
+}: FetcherParams) => {
+  const res = await fetch(url, {
+    method,
+    ...(body && { body: JSON.stringify(body) }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("API error");
+  }
+  if (json) {
+    const data = await res.json();
+    return data.data;
+  }
+};
+
+export const register = (user: { email: string; password: string }) =>
+  fetcher({
+    url: "/api/register",
+    method: "POST",
+    body: user,
+  });
+
+export const signin = (user: { email: string; password: string }) =>
+  fetcher({
+    url: "/api/signin",
+    method: "POST",
+    body: user,
+  });
